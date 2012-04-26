b9d3509949e3d9136b6dd31d90558ac0f90f4d4b
Manish Bellani <manish.bellani@gmail.com>
2012-04-26 01:14:42 -0500

    Add Sequence Identification Functionality

--- app/file_sequence.rb
+++ app/file_sequence.rb
@@ -1,19 +1,34 @@
 class FileSequence
   
   def initialize(file_name)
-    transform(file_name) if !file_name.nil?
+    @ext = file_name[file_name.rindex(".")+1..file_name.length] if !file_name.nil?
+    @file_name = transform(file_name) if !file_name.nil?
   end
   
-  def transform(file_name)
-    @file_name=file_name
-    @ext = @file_name[@file_name.rindex(".")+1..@file_name.length]
-    numbers_in_file_name = @file_name.scan(/\d+/)
-   if !numbers_in_file_name.nil? && !numbers_in_file_name.empty?
-     # Numbers towards the end of the file are considered the sequence number, so replace them 
-     sequence_number = numbers_in_file_name.last
-     @file_name=@file_name.sub(/#{Regexp.quote(sequence_number)}\.#{@ext}$/,"#{format(sequence_number)}.#{@ext}")
-   end
+  def file_name
+    @file_name
   end
+  
+  def falls_in_sequence?(file_name)
+    @file_name == (transform(file_name))
+  end
+  
+private
+
+  def transform(file_name)
+    sequence_number = extract_sequence_number(file_name)
+   if !sequence_number.nil?
+     file_name=file_name.sub(/#{Regexp.quote(sequence_number)}\.#{@ext}$/,"#{format(sequence_number)}.#{@ext}")
+   end
+   file_name
+  end
+  
+  def extract_sequence_number(file_name)
+    numbers_in_file_name = file_name.scan(/\d+/)
+    # Numbers towards the end of the file are considered the sequence number, so replace them 
+     (!numbers_in_file_name.nil? && !numbers_in_file_name.empty?)  ? numbers_in_file_name.last : nil
+  end
+
   
   def format(sequence_number)
     length_of_sequence_number=sequence_number.length
@@ -26,8 +41,5 @@ class FileSequence
     end
   end
   
-  def file_name
-    @file_name
-  end
   
 end
---
+++ test/file_sequence/test_sequence_identification_in_file_sequence.rb
@@ -0,0 +1,37 @@
+require 'test/unit'
+require_relative '../../app/file_sequence.rb'
+
+class TestSequenceIdentificationInFileSequence < Test::Unit::TestCase
+
+  
+  def test_should_consider_sd_fx_29_0102_rgb_in_sequence
+    file_sequence = FileSequence.new("sd_fx29.0101.rgb")
+    assert_equal(true,file_sequence.falls_in_sequence?("sd_fx29.0102.rgb"))
+  end
+  
+  def test_should_consider_sd_fx_29_0105_rgb_in_sequence
+    file_sequence = FileSequence.new("sd_fx29.0101.rgb")
+    assert_equal(true,file_sequence.falls_in_sequence?("sd_fx29.0105.rgb"))
+  end
+  
+  def test_not_should_consider_file03_2_rgb_in_sequence
+    file_sequence = FileSequence.new("sd_fx29.0101.rgb")
+    assert_equal(false,file_sequence.falls_in_sequence?("file03_2.rgb"))
+  end
+  
+  def test_not_should_consider_sd_fx_29_101_rgb_in_sequence
+    file_sequence = FileSequence.new("sd_fx29.0101.rgb")
+    assert_equal(false,file_sequence.falls_in_sequence?("sd_fx29.101.rgb"))
+  end
+  
+  def test_should_consider_file01_0042_rgb_in_sequence
+    file_sequence = FileSequence.new("file01_0040.rgb")
+    assert_equal(true,file_sequence.falls_in_sequence?("file01_0042.rgb"))
+  end
+  
+  def test_should_not_consider_a_txt_in_sequence
+    file_sequence = FileSequence.new("file01_0040.rgb")
+    assert_equal(false,file_sequence.falls_in_sequence?("a.txt"))
+  end
+  
+end
